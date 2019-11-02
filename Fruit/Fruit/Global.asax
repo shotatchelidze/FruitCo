edb5e4e1d9903ba41b2a21f2b257d509dfb3527f
shotatchelidze <shotikotchelidze@gmail.com>
2019-11-02 17:00:43 +0400

    Init2

---
+++ Fruit/Fruit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fruit.MvcApplication" Language="C#" %>
---
+++ Fruit/Fruit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fruit.Startup))]
+namespace Fruit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
